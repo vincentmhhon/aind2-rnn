464922b0edc175fdc30b64532b0ed21baa5067a5
vincentmhhon <vincentmhhon@gmail.com>
2017-08-28 20:12:14 +0800

    fix bug

--- my_answers.py
+++ my_answers.py
@@ -73,7 +73,5 @@ def build_part2_RNN(window_size, num_chars):
     model = Sequential()
     model.add(LSTM(200, input_shape=(window_size,num_chars)))
     model.add(Dense(num_chars))
-    model.add(Activation('relu'))
-    model.add(Dense(num_chars))
     model.add(Activation('softmax'))
     return model
