d821a15eb3c891c621e6e2782e8e145267a38602
vincentmhhon <vincentmhhon@gmail.com>
2017-08-28 04:02:43 -0400

    Enhancement finished window_transform_text function

--- my_answers.py
+++ my_answers.py
@@ -36,14 +36,13 @@ def build_part1_RNN(window_size):
     model.add(Dense(1))
     return model
 
-### DONE: return the text input with only ascii lowercase and the punctuation given below included.
+# DONE: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
-
+    punctuation = [' ', '!', ',', '.', ':', ';', '?']
     text = text.lower()
     text_unique_characters = ''.join(set(text))
     for c in text_unique_characters:
-        if c != ' ' and c not in string.ascii_lowercase and c not in punctuation:
+        if c not in string.ascii_lowercase and c not in punctuation:
             text = text.replace(c, '')
 
     return text
@@ -53,15 +52,19 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-
-    number_of_pairs = -(-len(text) // step_size)
     i = 0
-    while i < number_of_pairs:
-        start_index = i * step_size
+    start_index = 0
+    while start_index < len(text) - window_size:
         inputs.append(text[start_index: start_index + window_size])
         outputs.append(text[start_index + window_size: start_index + window_size + 1])
         i = i + 1
+        start_index = i * step_size
 
+    # reshape each
+    inputs = np.asarray(inputs)
+    inputs.shape = (np.shape(inputs)[0:2])
+    outputs = np.asarray(outputs)
+    outputs.shape = (np.shape(outputs)[0:2])
     return inputs,outputs
 
 # TODO build the required RNN model: 
