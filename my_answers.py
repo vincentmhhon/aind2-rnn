76ddde1b14e534de8e3090a1595996cce730d980
vincentmhhon <vincentmhhon@gmail.com>
2017-08-27 23:45:04 -0400

    Enhancement finished build_part1_RNN function and part 1 of project

--- my_answers.py
+++ my_answers.py
@@ -13,7 +13,7 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
-    y = series[2:]
+    y = series[window_size: ]
     series_size = len(series) - window_size
     i = 0
     while i < series_size:
@@ -28,10 +28,12 @@ def window_transform_series(series, window_size):
 
     return X,y
 
-# TODO: build an RNN to perform regression on our time series input/output data
+# DONE: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
